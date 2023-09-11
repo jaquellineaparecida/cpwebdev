0ae149c48c2b3d7e7857b6b507f72367a4cff6ca
jaquelline <jaquellineaparecida04@gmail.com>
2023-09-11 10:37:28 -0300

    margin

--- src/app/components/msguser.js
+++ src/app/components/msguser.js
@@ -1,7 +1,7 @@
 export default function MessageUser ({children}) {
     return (
         <main className="flex justify-end">
-            <div className="rounded-lg bg-blue-300 mt-3 w-36 h-10 mr-2 text-center">
+            <div className="rounded-lg bg-blue-300 mt-3 w-36 h-10 mr-3 text-center">
                 <p className="text-black text-[12px] font-semibold ml-2 pt-3 pr-2 w-auto h-auto text-center"> {children}  </p>
             </div>
         </main>
